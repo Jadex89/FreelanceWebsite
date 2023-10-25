87c78c1cc278804e0e7e07ec27a67734e03d1076
Jadex89 <141056820+Jadex89@users.noreply.github.com>
2023-10-25 18:35:22 +0100

    Added a popup modal on site launch using javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function ()  
+{
+    $(".close").on("click", function () 
+{
+    $("#modal").css("display", "none")
+	});
+  
+});
+
+if(sessionStorage.getItem('#modal') !== 'true'){
+    $('#modal').css('display','block');
+
+    sessionStorage.setItem('#ad_modal','true');
+	}
